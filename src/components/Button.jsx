c3f9269143009c8afe103bc287845470f035b91d
Moolmancell <richmondclaudebodot@gmail.com>
2024-12-16 14:52:04 +0800

    Add default values for callback prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-export function Button({callback, children}) {
+export function Button({callback = ()=>{}, children}) {
     return <button
         onClick={callback}
         className='border-solid border-2 border-black'
